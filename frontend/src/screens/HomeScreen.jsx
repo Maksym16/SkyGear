60a78e96fe0e462b20c273bba26ef9199a2de6ff
Maksym <mfedorenko16@gmail.com>
2025-01-23 05:09:20 -0500

    top products carousel set up

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,7 @@ import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import ProductCarousel from '../components/ProductCarousel';
 import { useGetProductsQuery } from '../slices/productApiSlice';
 import { useParams } from 'react-router-dom';
 import Paginate from '../components/Paginate';
@@ -16,7 +17,7 @@ const HomeScreen = () => {
   return (
     <>
     {!keyword ? (
-        <div />
+        <ProductCarousel />
       ) : (
         <Link to='/' className='btn btn-light mb-4'>
           Go Back
